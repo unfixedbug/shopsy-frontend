aa5672f7c0eb632ec9973bc1326d4a169dc42a7a
Dhiraj <jdhiraj543@gmail.com>
2021-12-16 18:24:26 +0530

    styled components added -W

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: "space-between";
+  align-items: center;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  aligh-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+  focus:none;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 const Navbar = () => {
-    return (
-        <div>
-            navbar
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center><Logo>SHOPSY</Logo></Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
