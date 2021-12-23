e270b52eda25d22e26fd48be3e5518d6a927ced4
Dhiraj <jdhiraj543@gmail.com>
2021-12-23 18:26:51 +0530

    login scenes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+
+import { PersistGate } from "redux-persist/integration/react";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}></PersistGate>
     <App />
   </Provider>,
   document.getElementById("root")
